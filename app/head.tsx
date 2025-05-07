36f01dc0b4dcf10691331a8d3fb677f76d363e14
eriblo92 <erik.blomgreen@gmail.com>
2025-05-07 19:18:39 +0200

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>CustomHedge</title>
+      <meta name="description" content="Professional derivatives hedging tailored to your holdings" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </>
+  );
+}
